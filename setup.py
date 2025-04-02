64f5aafffee7d66da6f2f0091bc03f6cb984fb1b
James McDermott <jamesmichaelmcdermott@gmail.com>
2025-04-02 07:48:59 +0100

    correct package name in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="GPAFS",
+    name="Magpie",
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     url='https://github.com/jmmcd',
     author='James McDermott',
     author_email='jamesmichaelmcdermott@gmail.com',
-    description='Grammatical Pareto Archive Function Synthesis',
+    description='Magpie: multi-objective archive genetic programming (for symbolic regression) from Ireland',
     install_requires=[
         "numpy",
         "pandas",
@@ -18,6 +18,6 @@ setup(
     ],
     include_package_data=True,
     package_data={
-        "GPAFS": ["data/*", "grammars/*"]
+        "Magpie": ["data/*", "grammars/*"]
     },
 )
